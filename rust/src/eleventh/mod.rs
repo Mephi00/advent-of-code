2a747c8e2d7d64883422caacc795a13791f44346
Mephi00 <maximhammer@icloud.com>
2022-12-12 15:34:56 +0100

    done day 11

--- rust/src/eleventh/mod.rs
+++ rust/src/eleventh/mod.rs
@@ -1,28 +1,125 @@
-use std::{
-    borrow::BorrowMut,
-    cell::{Cell, RefCell},
-    sync::Arc,
-};
+use core::cmp::Ordering;
+use std::cell::RefCell;
+
+use num_bigint::BigUint;
 
 use crate::eleventh::monkey::Monkey;
 
 mod monkey;
 
 pub fn main(input_str: &String) {
-    let input_lines = input_str.split("\n");
+    let input_lines: Vec<&str> = input_str.split("\n").collect();
 
-    let mut monkeys: Vec<Cell<Monkey>> = Vec::new();
+    let mut monkeys = read_input(input_lines.clone());
+
+    let mut monkeys_second = read_input(input_lines);
+
+    let min_div = monkeys
+        .iter()
+        .map(|x| x.borrow().test_num)
+        .fold(1, |a, b| a * b);
+
+    let min_div_second = monkeys_second
+        .iter()
+        .map(|x| x.borrow().test_num)
+        .fold(1, |a, b| a * b);
+
+    do_turns(&monkeys, 20, true, min_div);
+
+    do_turns(&monkeys_second, 10000, false, min_div_second);
+
+    println!("len of monkey vec: {}", monkeys.len());
+
+    monkeys.sort_by(|a, b| {
+        if a.borrow().num_inspected_items > b.borrow().num_inspected_items {
+            return Ordering::Less;
+        } else if a.borrow().num_inspected_items == b.borrow().num_inspected_items {
+            return Ordering::Equal;
+        } else {
+            return Ordering::Greater;
+        }
+    });
+
+    monkeys_second.sort_by(|a, b| {
+        if a.borrow().num_inspected_items > b.borrow().num_inspected_items {
+            return Ordering::Less;
+        } else if a.borrow().num_inspected_items == b.borrow().num_inspected_items {
+            return Ordering::Equal;
+        } else {
+            return Ordering::Greater;
+        }
+    });
+
+    println!(
+        "monkey busines of top monkey: {}",
+        monkeys_second[0].borrow().num_inspected_items
+    );
+
+    println!(
+        "monkey busines of second monkey: {}",
+        monkeys_second[1].borrow().num_inspected_items
+    );
+
+    println!(
+        "total monkey business of top 2 after 20 iterations: {}",
+        monkeys[0].borrow().num_inspected_items * monkeys[1].borrow().num_inspected_items
+    );
+
+    println!(
+        "total monkey business of top 2 after 1000 iterations: {}",
+        monkeys_second[0].borrow().num_inspected_items
+            * monkeys_second[1].borrow().num_inspected_items
+    );
+}
+
+fn do_turns(
+    monkeys: &Vec<RefCell<Monkey>>,
+    num_iter: u32,
+    include_reduction: bool,
+    min_divisor: u128,
+) {
+    for _ in 0..num_iter {
+        for i in 0..monkeys.len() {
+            let mut monkey = monkeys[i].borrow_mut();
+            let test_true_num = monkey.test_true;
+            let test_false_num = monkey.test_false;
+
+            let mut test_true = monkeys[test_true_num].borrow_mut();
+
+            if test_true_num == test_false_num {
+                if include_reduction {
+                    monkey.turn(&mut test_true, None, min_divisor);
+                } else {
+                    monkey.turn_without_reduction(&mut test_true, None, min_divisor);
+                }
+            }
+
+            let mut test_false = monkeys[test_false_num].borrow_mut();
+            if include_reduction {
+                monkey.turn(&mut test_true, Some(&mut test_false), min_divisor);
+            } else {
+                monkey.turn_without_reduction(&mut test_true, Some(&mut test_false), min_divisor);
+            }
+        }
+    }
+}
+
+fn read_input(input_list: Vec<&str>) -> Vec<RefCell<Monkey>> {
+    let mut monkeys: Vec<RefCell<Monkey>> = Vec::new();
 
     let mut test_num = 1;
     let mut test_true = 0;
     let mut test_false = 0;
-    let mut starting_items: Vec<u32> = Vec::new();
-    let mut operation: Box<dyn Fn(u32) -> u32> = Box::new(|_| 0);
+    let mut starting_items: Vec<BigUint> = Vec::new();
+    let mut operation: Box<dyn Fn(&BigUint) -> BigUint> = Box::new(|_| BigUint::default());
     let mut monkey_line_counter = 0;
 
-    for line in input_lines {
+    for (idx, line) in input_list.iter().enumerate() {
         match monkey_line_counter {
-            0 => continue,
+            0 => {
+                monkey_line_counter += 1;
+                continue;
+            }
             1 => {
                 starting_items = line
                     .split(":")
@@ -41,24 +138,42 @@ pub fn main(input_str: &String) {
                     .map(|f| f.trim())
                     .collect();
 
-                let number: u32 = function[2]
-                    .parse()
-                    .expect(&format!("couldnt parse {}", function[2]));
+                if function[2] == "old" {
+                    match function[1] {
+                        "+" => {
+                            operation = Box::new(move |input| input.clone() + input);
+                        }
+                        "-" => {
+                            operation = Box::new(move |input| input.clone() - input);
+                        }
+                        "*" => {
+                            operation = Box::new(move |input| input.clone() * input);
+                        }
+                        "/" => {
+                            operation = Box::new(move |input| input.clone() / input);
+                        }
+                        _ => {}
+                    }
+                } else {
+                    let number: u128 = function[2]
+                        .parse()
+                        .expect(&format!("couldnt parse {}", function[2]));
 
-                match function[1] {
-                    "+" => {
-                        operation = Box::new(move |input| input + number);
+                    match function[1] {
+                        "+" => {
+                            operation = Box::new(move |input| input + number);
+                        }
+                        "-" => {
+                            operation = Box::new(move |input| input - number);
+                        }
+                        "*" => {
+                            operation = Box::new(move |input| input * number);
+                        }
+                        "/" => {
+                            operation = Box::new(move |input| input / number);
+                        }
+                        _ => {}
                     }
-                    "-" => {
-                        operation = Box::new(move |input| input - number);
-                    }
-                    "*" => {
-                        operation = Box::new(move |input| input * number);
-                    }
-                    "/" => {
-                        operation = Box::new(move |input| input / number);
-                    }
-                    _ => {}
                 }
             }
             3 => {
@@ -89,8 +204,8 @@ pub fn main(input_str: &String) {
 
         // increase line count and test if input reset and push monkey to list
         monkey_line_counter += 1;
-        if line.is_empty() {
-            monkeys.push(Cell::new(Monkey::new(
+        if line.is_empty() || idx == input_list.len() - 1 {
+            monkeys.push(RefCell::new(Monkey::new(
                 starting_items,
                 operation,
                 test_num,
@@ -102,17 +217,10 @@ pub fn main(input_str: &String) {
             test_false = 0;
             test_true = 0;
             starting_items = Vec::new();
-            operation = Box::new(|_| 0);
+            operation = Box::new(|_| BigUint::default());
             monkey_line_counter = 0;
         }
     }
 
-    for _ in 0..20 {
-        for i in 0..monkeys.len() {
-            let monkey = monkeys[i].get_mut();
-            let test_true = monkeys[monkey.test_true].get_mut();
-            let test_false = monkeys[monkey.test_false].get_mut();
-            monkey.turn(&mut test_true, &mut test_false);
-        }
-    }
+    monkeys
 }
--- rust/src/eleventh/monkey.rs
+++ rust/src/eleventh/monkey.rs
@@ -1,23 +1,25 @@
+use num_bigint::{BigUint, ToBigUint};
+
 pub struct Monkey {
-    items: Vec<u32>,
-    operation: Box<dyn Fn(u32) -> u32>,
-    test_num: u32,
+    items: Vec<BigUint>,
+    operation: Box<dyn Fn(&BigUint) -> BigUint>,
+    pub test_num: u128,
     pub test_true: usize,
     pub test_false: usize,
-    pub num_inspected_items: u64,
+    pub num_inspected_items: u128,
 }
 
 impl Monkey {
     pub fn new(
-        starting_items: Vec<u32>,
-        operation: Box<dyn Fn(u32) -> u32>,
-        test_num: u32,
+        starting_items: Vec<BigUint>,
+        operation: Box<dyn Fn(&BigUint) -> BigUint>,
+        test_num: u128,
         test_true: usize,
         test_false: usize,
     ) -> Monkey {
         Monkey {
             items: starting_items,
-            operation: Box::new(operation),
+            operation: operation,
             test_num: test_num,
             test_false: test_false,
             test_true: test_true,
@@ -25,22 +27,74 @@ impl Monkey {
         }
     }
 
-    pub fn receive_item(&mut self, item: u32) {
+    pub fn receive_item(&mut self, item: BigUint) {
         self.items.push(item);
     }
 
-    pub fn turn(&mut self, monkey_true: &mut Self, monkey_false: &mut Self) {
-        for item in &self.items {
-            self.num_inspected_items += 1;
-            let new_worry = (self.operation)(*item) / 3;
+    pub fn turn(
+        &mut self,
+        monkey_true: &mut Self,
+        monkey_false: Option<&mut Self>,
+        min_divisor: u128,
+    ) {
+        let different_monkeys = monkey_false.as_ref().is_some();
+        if different_monkeys {
+            let false_monkey = monkey_false.unwrap();
 
-            if new_worry % self.test_num == 0 {
+            for item in &self.items {
+                self.num_inspected_items += 1;
+                let mut new_worry = (self.operation)(item) / 3.to_biguint().unwrap();
+
+                new_worry %= min_divisor;
+
+                if &new_worry % self.test_num == 0.to_biguint().unwrap() {
+                    monkey_true.receive_item(new_worry);
+                } else {
+                    false_monkey.receive_item(new_worry);
+                }
+            }
+        } else {
+            for item in &self.items {
+                self.num_inspected_items += 1;
+                let new_worry = (self.operation)(item) / 3.to_biguint().unwrap();
+
                 monkey_true.receive_item(new_worry);
-            } else {
-                monkey_false.receive_item(new_worry);
             }
         }
+        self.items = Vec::new();
+    }
 
+    pub fn turn_without_reduction(
+        &mut self,
+        monkey_true: &mut Self,
+        monkey_false: Option<&mut Self>,
+        min_divisor: u128,
+    ) {
+        // let different_monkeys = monkey_false.as_ref().is_some();
+        // if different_monkeys {
+        let false_monkey = monkey_false.unwrap();
+
+        for item in &self.items {
+            self.num_inspected_items += 1;
+            let mut new_worry = (self.operation)(item);
+
+            new_worry %= min_divisor;
+            if &new_worry % self.test_num == 0.to_biguint().unwrap() {
+                monkey_true.receive_item(new_worry);
+            } else {
+                false_monkey.receive_item(new_worry);
+            }
+        }
+        // } else {
+        //     for item in &self.items {
+        //         self.num_inspected_items += 1;
+        //         let mut new_worry = (self.operation)(item);
+
+        //         // new_worry %= min_divisor;
+
+        //         monkey_true.receive_item(new_worry);
+        //     }
+        // }
         self.items = Vec::new();
     }
 }
